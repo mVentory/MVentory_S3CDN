6f6577e180eb8e95db08ff187e6de563e543544f
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2014-03-26 14:21:12 +0800

    Add licence and copyright notices

--- code/Block/System/Config/Form/Field/Placeholders.php
+++ code/Block/System/Config/Form/Field/Placeholders.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE-OSL.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @package MVentory/CDN
+ * @copyright Copyright (c) 2014 mVentory Ltd. (http://mventory.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Button for placeholders uploading
+ *
+ * @package MVentory/Productivity
+ * @author Anatoly A. Kazantsev <anatoly@mventory.com>
+ */
 class MVentory_CDN_Block_System_Config_Form_Field_Placeholders
   extends Mage_Adminhtml_Block_System_Config_Form_Field {
 
--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE-OSL.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @package MVentory/CDN
+ * @copyright Copyright (c) 2014 mVentory Ltd. (http://mventory.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Data helper
+ *
+ * @package MVentory/Productivity
+ * @author Anatoly A. Kazantsev <anatoly@mventory.com>
+ */
 class MVentory_CDN_Helper_Data extends Mage_Core_Helper_Abstract {
 
   private $_prefix = null;
